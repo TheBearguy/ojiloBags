823661073f5033bb140f134af760764652f17413
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-06-30 17:27:57 +0530

    utils - wrapping requests in promises, custom errors, custom resopnses

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// Wrap the request in a promise and catch any errors that occur during the request.
+
+// If an error occurs, pass it to the next middleware function.
+
+const asyncHandler = (requestHandler) => {
+
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch( (err) => next(err))
+    }
+}
+
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next)
+//         .catch(next);
+//     } catch (error) {
+//         res.status(error.status || 500).json({
+//             message: error.message,
+//             success: false
+//         })
+//     }
+// }
+
+export default asyncHandler;
